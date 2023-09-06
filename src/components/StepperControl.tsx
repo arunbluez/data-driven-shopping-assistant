9677510548ee4be004f2a4930e2d0e3920774d36
arunbluez <arunbluez@gmail.com>
2023-09-06 13:19:26 +0200

    :green_heart: CI build

--- src/components/StepperControl.tsx
+++ src/components/StepperControl.tsx
@@ -1,7 +1,3 @@
-import React from "react"
-
-type Props = {}
-
-export default function StepperControl({}: Props) {
+export default function StepperControl() {
   return <div>StepperControl</div>
 }
