f0803c1419c26ccc041b8450a492bb83975b7737
Darius Bock <bockdar@mediamarktsaturn.com>
2023-09-07 12:33:53 +0200

    📱 Make responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,23 @@
-import AppBar from "@mui/material/AppBar"
-import Box from "@mui/material/Box"
-import Toolbar from "@mui/material/Toolbar"
-import mmsLogo from "../assets/MMSE_Logo.svg"
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import mmsLogo from '../assets/MMSE_Logo.svg';
 
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position='static'>
         <Toolbar>
-          <div className="w-full px-8 flex items-center justify-between">
-            <img src={mmsLogo} alt="logo" width={200} />
-            <p className="text-4xl font-black">
-              <span className="font-light">TEAM</span>11
+          <div className='w-full px-2 md:px-8 flex items-center justify-between'>
+            <div className='flex flex-shrink-0 items-center'>
+              <img className='h-8 w-auto' src={mmsLogo} alt='MMS' />
+            </div>
+            <p className='text-2xl font-black'>
+              <span className='font-light'>TEAM</span>11
             </p>
           </div>
         </Toolbar>
       </AppBar>
     </Box>
-  )
+  );
 }
--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,79 +1,81 @@
-import { Rating } from "@mui/material"
-import EnergyLabel from "./EnergyLabel/EnergyLabel"
-import { Product } from "../product/product"
+import { Rating } from '@mui/material';
+import EnergyLabel from './EnergyLabel/EnergyLabel';
+import { Product } from '../product/product';
 
 type Props = {
-  product: Product
-}
+  product: Product;
+};
 export default function ProductItem({ product }: Props) {
   return (
-    <div className="w-full border border-gray-400 rounded-xl p-4">
-      <div className="flex flex-col gap-2">
-        <div className="flex justify-between items-center w-full">
-          <p className="font-bold">{product.name}</p>
+    <div className='w-full border border-gray-400 rounded-xl p-4'>
+      <div className='flex flex-col gap-2'>
+        <div className='flex justify-between items-center'>
+          <p className='font-bold text-left'>{product.name}</p>
           <EnergyLabel />
         </div>
-        <div className="flex gap-4">
-          <div className="flex flex-col justify-between w-1/3 p-4">
-            <div className="w-full flex items-center justify-center">
-              <img width="80%" src={product.imageUrl} />{" "}
+        <div className='flex flex-col md:flex-row justify-start gap-4'>
+          <div className='flex flex-col justify-between md:w-1/3 gap-2 p-4'>
+            <div className='w-full flex justify-start sm:justify-center'>
+              <img width='100%' src={product.imageUrl} />
             </div>
-            <div className="flex items-center gap-2">
+            <div className='flex items-center gap-2'>
               <Rating
-                name="read-only"
+                name='read-only'
                 value={product.reviews.average}
                 precision={0.25}
                 readOnly
-                color="primary"
+                color='primary'
               />
               <p>{product.reviews.count}</p>
             </div>
           </div>
-          <div className="flex flex-col gap-2 items-start justify-start p-4 w-1/3">
-            {product.topFeatures.map((feature) => (
-              <div className="text-start">
-                <p className="text-sm">{feature.name}</p>
-                <p className="font-bold">{feature.value}</p>
-              </div>
-            ))}
-          </div>
-          <div className="flex flex-col gap-4 items-start justify-start p-4">
-            <div className="text-start">
-              <p className="text-3xl font-bold">
-                {product.price.value.toFixed(0)},-
-              </p>
-              <div className="text-xs text-start">
-                <p>inkl. MwSt. versandkostenfrei</p>
-                <p>
-                  Bezahle in 12 Raten à 26,30 € (eff. Zins. P.a. 9.9%)**
-                </p>{" "}
-                <p>Gesamtpreis 315.6 €</p>
-              </div>
+          <div className='flex flex-col sm:flex-row justify-between'>
+            <div className='flex flex-col gap-2 items-start justify-start p-4'>
+              {product.topFeatures.map((feature) => (
+                <div className='text-start'>
+                  <p className='text-sm'>{feature.name}</p>
+                  <p className='font-bold'>{feature.value}</p>
+                </div>
+              ))}
             </div>
+            <div className='flex flex-col gap-4 items-start justify-start p-4'>
+              <div className='text-start'>
+                <p className='text-3xl font-bold'>
+                  {product.price.value.toFixed(0)},-
+                </p>
+                <div className='text-xs text-start'>
+                  <p>inkl. MwSt. versandkostenfrei</p>
+                  <p>
+                    Bezahle in 12 Raten à 26,30 € (eff. Zins. P.a. 9.9%)**
+                  </p>{' '}
+                  <p>Gesamtpreis 315.6 €</p>
+                </div>
+              </div>
 
-            <div className="text-start">
-              <div className="flex items-center gap-2">
-                <div className="bg-green-700 rounded-full w-2 h-2"></div>
-                <p className="text-green-700 text-xs font-bold">
-                  Online verfügbar
+              <div className='text-start'>
+                <div className='flex items-center gap-2'>
+                  <div className='bg-green-700 rounded-full w-2 h-2'></div>
+                  <p className='text-green-700 text-xs font-bold'>
+                    Online verfügbar
+                  </p>
+                </div>
+                <p className='text-gray-600 text-xs pl-4'>
+                  Lieferung 07.09.2023 - 08.09.2023
                 </p>
               </div>
-              <p className="text-gray-600 text-xs pl-4">
-                Lieferung 07.09.2023 - 08.09.2023
-              </p>
-            </div>
-            <div className="text-start">
-              <div className="flex items-center gap-2">
-                <div className="bg-green-700 rounded-full w-2 h-2"></div>
-                <p className="text-green-700 text-xs font-bold">
-                  Marktabholung ab 09.09.2023
-                </p>
+              <div className='text-start'>
+                <div className='flex items-center gap-2'>
+                  <div className='bg-green-700 rounded-full w-2 h-2'></div>
+                  <p className='text-green-700 text-xs font-bold'>
+                    Marktabholung ab 09.09.2023
+                  </p>
+                </div>
+                <p className='text-gray-600 text-xs pl-4'>Ingolstadt</p>
               </div>
-              <p className="text-gray-600 text-xs pl-4">Ingolstadt</p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
